af2664031a1158d209fbeb2f4405807fae53190a
Ajiva-D <davidenochaji@gmail.com>
2019-01-10 12:36:08 +0100

    Added scroll highlight for Price plans on mobile devices

--- js/jquery.js
+++ js/jquery.js
@@ -60,61 +60,103 @@ $(function () {
         }
     });
 
-    $(window).scroll(function () {
-        var hT = $('.starter').offset().top,
-            hH = $('.starter').outerHeight(),
-            wH = $(window).height(),
-            wS = $(this).scrollTop();
-        console.log((hT - wH), wS);
-        if (wS > (hT + hH - wH)) {
-            $(".starter").slideDown(2000);
+    // $(window).scroll(function () {
+    //     var hT = $('.starter').offset().top,
+    //         hH = $('.starter').outerHeight(),
+    //         wH = $(window).height(),
+    //         wS = $(this).scrollTop();
+    //     console.log((hT - wH), wS);
+    //     if (wS > (hT + hH - wH)) {
+    //         $(".starter").slideDown(2000);
 
-        }
-    });
-    $(window).scroll(function () {
-        var hT = $('.bas-adv').offset().top,
-            hH = $('.bas-adv').outerHeight(),
-            wH = $(window).height(),
-            wS = $(this).scrollTop();
-        console.log((hT - wH), wS);
-        if (wS > (hT + hH - wH)) {
-            $(".bas-adv").slideDown(2000);
+    //     }
+    // });
+    // $(window).scroll(function () {
+    //     var hT = $('.bas-adv').offset().top,
+    //         hH = $('.bas-adv').outerHeight(),
+    //         wH = $(window).height(),
+    //         wS = $(this).scrollTop();
+    //     console.log((hT - wH), wS);
+    //     if (wS > (hT + hH - wH)) {
+    //         $(".bas-adv").slideDown(2000);
 
-        }
-    });
+    //     }
+    // });
 
     // Content for the about menu
 
-    $("#fast").click(function(){
+    $("#fast").click(function () {
         $("#about-content").html("<p>One of the interesting things is that quite a lot of my background is betrayed by the various options that's portrayed to me.</p><p>I wonder how many readers will have thought of the Times Literary Supplement, for example. I'm also more likely to think of sme as the term relating to organisations, because that's the favoured form in Europe, whereas I believ that the US tends to SMB.</p><p>I'm sure your experiences will all be different-which rather makes my point for me.</p>");
         $(".sub-menu").children().removeClass('menu-active');
         $(this).addClass('menu-active');
     });
 
-   $("#reliable").click(function(){
+    $("#reliable").click(function () {
         $("#about-content").html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>");
         $(".sub-menu").children().removeClass('menu-active');
         $(this).addClass('menu-active');
     });
 
-   $("#intuitive").click(function(){
+    $("#intuitive").click(function () {
         $("#about-content").html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>");
         $(".sub-menu").children().removeClass('menu-active');
         $(this).addClass('menu-active');
     });
 
-    $("#secure").click(function(){
+    $("#secure").click(function () {
         $("#about-content").html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam,</p><p>Hello to the whole world, lets just let them know that's whats up with me and you and I have no idea what im typing just feel it should make sense. If it doesn't just bear with me, can you do that?</p>");
         $(".sub-menu").children().removeClass('menu-active');
         $(this).addClass('menu-active');
     });
 
-    $("#team").click(function(){
+    $("#team").click(function () {
         $("#about-content").html("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>");
         $(".sub-menu").children().removeClass('menu-active');
         $(this).addClass('menu-active');
     });
 
+
+    $(window).scroll(function () {
+        var position = window.pageYOffset;
+        $('#starter').each(function () {
+            var target = $(this).offset().top;
+            if ((position >= target) && ($(window).width() <= 320)) {
+                $("#starter").addClass('plans-active');
+                $("#basic").removeClass('plans-active');
+                $("#advanced").removeClass('plans-active');
+            }
+            else if($('#starter').hasClass("plans-active") && ($(window).width() <= 320)){
+                $("#starter").removeClass('plans-active');
+            }
+        });
+
+    });
+
+    $(window).scroll(function () {
+        var position = window.pageYOffset;
+        $('#basic').each(function () {
+            var target = $(this).offset().top;
+            if ((position >= target) && ($(window).width() <= 320)) {
+                $("#basic").addClass('plans-active');
+                $("#starter").removeClass('plans-active');
+                $("#advanced").removeClass('plans-active');
+            }
+            
+        });
+    });
+
+    $(window).scroll(function () {
+        var position = window.pageYOffset;
+        $('#advanced').each(function () {
+            var target = $(this).offset().top;
+            if ((position >= target) && ($(window).width() <= 320)) {
+                $("#advanced").addClass('plans-active');
+                $("#basic").removeClass('plans-active');
+                $("#starter").removeClass('plans-active');
+            }
+            
+        });
+    });
     // var onScroll = function() {
     //     var scrollTop = $(this).scrollTop();
     //     $('.paralax-image').each(function(index, elem) {
